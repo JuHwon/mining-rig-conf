5f9250b7a67a63d9c56b7aa58199c2314fdfbf25
JuHwon <juer.wagner@gmail.com>
2017-12-07 12:09:15 +0100

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y vim tmux git
 
 sudo update-grub
 
-sudo nvidia-xconfig -a --cool-bits=28 --enable-empty-initial-configuration
+sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
 
 sudo systemctl restart lightdm.service
 
